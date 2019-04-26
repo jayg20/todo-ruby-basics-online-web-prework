a07f94610667a0c512a1571b500d9fdb5585abd0
James Gomez <james.gomez320@yahoo.com>
2019-04-26 14:11:35 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,21 +1,22 @@
 def division(num1, num2)
- 
+ num1/num2= total
+ puts total
 end
 
-def assign_variable(value)
-  
+def assign_variable(value="Bob")
+  puts "#{value}"
 end
 
-def argue
-  
+def argue(phrase="I'm right and you're wrong!")
+  puts "#{phrase}"
 end
 
-def greeting
-
+def greeting(greeting="Hi there," name="Bobby!")
+  puts "#{greeting}, #{name}"
 end
 
-def return_a_value
-
+def return_a_value()
+  return "NIce!"
 end
 
 def last_evaluated_value
